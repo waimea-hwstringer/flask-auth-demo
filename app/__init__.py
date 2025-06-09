895c452dbfd5c58113688c5fc2848440133c8dd1
Harry Stringa <hwstringer@waimea.school.nz>
2025-06-10 09:10:57 +1200

    cpy error handling changes

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 import html
 
 from app.helpers.session import init_session
-from app.helpers.db import connect_db, handle_db_errors
+from app.helpers.db import connect_db
 from app.helpers.errors import register_error_handlers, not_found_error
 
 
@@ -47,7 +47,7 @@ def signup():
 # Things page route - Show all the things, and new thing form
 #-----------------------------------------------------------
 @app.get("/things/")
-@handle_db_errors
+ 
 def show_all_things():
     with connect_db() as client:
         # Get all the things from the DB
@@ -63,7 +63,7 @@ def show_all_things():
 # Thing page route - Show details of a single thing
 #-----------------------------------------------------------
 @app.get("/thing/<int:id>")
-@handle_db_errors
+ 
 def show_one_thing(id):
     with connect_db() as client:
         # Get the thing details from the DB
@@ -96,7 +96,7 @@ def show_one_thing(id):
 # Route for adding a thing, using data posted from a form
 #-----------------------------------------------------------
 @app.post("/add")
-@handle_db_errors
+ 
 def add_a_thing():
     # Get the data from the form
     name  = request.form.get("name")
@@ -120,7 +120,7 @@ def add_a_thing():
 # Route for adding a user, using data posted from a form
 #-----------------------------------------------------------
 @app.post("/add-user")
-@handle_db_errors
+ 
 def add_a_user():
     # Get the data from the form
     name  = request.form.get("name")
@@ -149,7 +149,7 @@ def add_a_user():
 # Route for deleting a thing, Id given in the route
 #-----------------------------------------------------------
 @app.get("/delete/<int:id>")
-@handle_db_errors
+ 
 def delete_a_thing(id):
     with connect_db() as client:
         # Delete the thing from the DB
